568dec1a5487b87652d1ed09af35117c5675690f
Johan Swan <jswannieman@gmail.com>
2023-11-20 15:03:39 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 function App() {
   return (
     <div>
-      <h2>
+      <h2 id="heading">
         Learn more about the React Testing Library and become a top Front End
         Developer
       </h2>
+      <h3 id="subheading">
+        The more you learn, the better equipped you will become
+      </h3>
     </div>
   );
 }
