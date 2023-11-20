a1f552633b1853bf77bf12d92cf7da27f1bd1ba2
Johan Swan <jswannieman@gmail.com>
2023-11-20 14:24:43 +1300

    Commit App JS Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2>
+        Learn more about the React Testing Library and become a top Front End
+        Developer
+      </h2>
+    </div>
+  );
+}
+
+export default App;
